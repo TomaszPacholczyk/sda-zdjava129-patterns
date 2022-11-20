63e42b198cd8bdf081d6dfd6dc38394595fd125f
Tomasz Pacholczyk <tomasz.pacholczyk@o2.pl>
2022-11-20 12:08:05 +0100

    Person, Student, Teacher, HasPerson

---
+++ src/main/java/pl/sdaacademy/java/patterns/HasPerson.java
@@ -0,0 +1,5 @@
+package pl.sdaacademy.java.patterns;
+
+interface HasPerson {
+    Person getPerson();
+}
---
+++ src/main/java/pl/sdaacademy/java/patterns/Teacher.java
@@ -0,0 +1,18 @@
+package pl.sdaacademy.java.patterns;
+
+import java.util.Set;
+
+class Teacher implements HasPerson {
+
+    private Person person;
+    private Set<String> subjectCodes;
+
+    @Override
+    public Person getPerson() {
+        return person;
+    }
+
+    public Set<String> getSubjectCodes() {
+        return subjectCodes;
+    }
+}
